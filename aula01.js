8d0a55637dc5dab485b440b3a15410a783d46835
SergioMotta999 <serginhomota77@gmail.com>
2023-06-22 22:54:03 -0300

    aula 1 a 4

---
+++ aula01.js
@@ -0,0 +1,5 @@
+//Commentaries em Js usa o //
+
+console.log("Hello World"); // imprime no console
+
+/*Comentario de uma barra de código*/ 
---
+++ aula02.js
@@ -0,0 +1,7 @@
+/*"use strict" Usado para deixar no modo restrito
+exemplo sem ele conseguimos usar o let nome = "Sejux"
+sem o (let)
+*/
+let nome = "Sejux"
+console.log(nome)
+console.log("Aula 02 do: " +nome)
